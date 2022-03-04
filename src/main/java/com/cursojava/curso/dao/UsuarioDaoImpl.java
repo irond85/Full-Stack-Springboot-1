88ddddecc283fb1bb4e3c49a511928a883aeeeac
IronDev <ofocacion.34@gmail.com>
2022-03-04 10:50:48 -0500

    Add maven dependency for @transactional

--- src/main/java/com/cursojava/curso/dao/UsuarioDaoImpl.java
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImpl.java
@@ -2,10 +2,13 @@ package com.cursojava.curso.dao;
 
 import com.cursojava.curso.models.Usuario;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
+@Transactional
 public class UsuarioDaoImpl implements UsuarioDao{
 
     EntityManager entityManager;
