a45110a2e447eaa00c91c32f60057fe4173489d9
Sergio <sergio.agudelo@artifex.co>
2022-03-07 18:41:16 -0500

    Se crea front del login

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    //Cuando este cargada
+});
+  
+async function iniciarSesion() {
+    let datos = {};
+
+    datos.email = document.getElementById('txtEmail').value;
+    datos.password = document.getElementById('txtPassword').value;    
+    
+    const request = await fetch('api/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    });
+
+    const response = await request.json();
+}
